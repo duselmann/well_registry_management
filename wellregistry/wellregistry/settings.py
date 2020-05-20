5d672f8e70b2ceefd8c34514f8d92c474db3eb0d
David Uselmann <duselmann@usgs.gov>
2020-05-20 10:54:27 -0500

    lookups template

--- wellregistry/wellregistry/settings.py
+++ wellregistry/wellregistry/settings.py
@@ -68,7 +68,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates')
+            os.path.join(PROJECT_DIR, 'templates'),
+            os.path.join(PROJECT_DIR, '../lookups/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
